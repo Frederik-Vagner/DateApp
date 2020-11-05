7bbf49e8616309ed14c59f6d3d19a1169f09cb90
Frederik-Vagner <70664117+Frederik-Vagner@users.noreply.github.com>
2020-11-05 12:26:20 +0100

    start

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping ("/")
+    public String welcome() {
+        return ("welcome");
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return ("home");
+    }
+
+
+    @GetMapping("/administrator")
+    public String administrator() {
+        return ("administrator");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("user");
+    }
+
+    //bruger
+
+    @GetMapping("/findUsers")
+    public String findUsers() {
+        return ("findUsers");
+    }
+
+    @GetMapping("/candidateList")
+    public String candidateList() {
+        return ("candidateList");
+    }
+
+    @GetMapping("/messages")
+    public String messages() {
+        return ("messages");
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return ("profile");
+    }
+
+    //footer
+
+    @GetMapping("/privacyPolicy")
+    public String privacyPolicy() {
+        return ("privacyPolicy");
+    }
+
+
+}
+
